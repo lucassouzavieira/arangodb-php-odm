d581477b831c684bf80b18c63b181153d470e5ad
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2019-10-21 17:01:19 -0300

    Add Server::version() method

---
+++ src/Admin/Admin.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace ArangoDB\Admin;
+
+/**
+ * Manages some Admin features
+ *
+ * @package ArangoDB\Admin
+ * @author Lucas S. Vieira
+ */
+abstract class Admin
+{
+
+}
