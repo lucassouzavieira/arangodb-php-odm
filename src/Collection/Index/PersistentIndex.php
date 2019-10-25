c4166abb499df1a17c51c76508094f9585cd39b5
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2019-10-25 17:19:34 -0300

    Implements 'PersistentIndex' class

--- src/Collection/Index/PersistentIndex.php
+++ src/Collection/Index/PersistentIndex.php
@@ -1,7 +1,9 @@
 <?php
-
+declare(strict_types=1);
 
 namespace ArangoDB\Collection\Index;
+
+use ArangoDB\Validation\Exceptions\InvalidParameterException;
 
 /**
  * Class PersistentIndex
@@ -10,6 +12,36 @@ namespace ArangoDB\Collection\Index;
  * @package ArangoDB\Collection\Index
  * @author Lucas S. Vieira
  */
-class PersistentIndex extends Index
+final class PersistentIndex extends Index
 {
+    /**
+     * PersistentIndex constructor.
+     *
+     * @param array $fields
+     * @param bool $unique
+     * @param bool $sparse
+     *
+     * @throws InvalidParameterException
+     */
+    public function __construct(array $fields, bool $unique = true, bool $sparse = true)
+    {
+        parent::__construct("persistent", $fields);
+        $this->unique = $unique;
+        $this->sparse = $sparse;
+    }
+
+    /**
+     * Return data for create index on server
+     *
+     * @return array
+     */
+    public function getCreateData(): array
+    {
+        return [
+            'type' => $this->getType(),
+            'fields' => $this->getFields(),
+            'unique' => $this->isUnique(),
+            'sparse' => $this->isSparse(),
+        ];
+    }
 }
---
+++ tests/Collection/Index/PersistentIndexTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Unit\Collection\Index;
+
+use Unit\TestCase;
+use ArangoDB\Collection\Index\PersistentIndex;
+
+class PersistentIndexTest extends TestCase
+{
+    public function setUp(): void
+    {
+        $this->loadEnvironment();
+        parent::setUp();
+    }
+
+    public function testConstructor()
+    {
+        $index = new PersistentIndex(['my_persistent_attr']);
+
+        $this->assertTrue($index->isNew());
+        $this->assertTrue($index->isUnique());
+        $this->assertEquals("persistent", $index->getType());
+    }
+
+    public function testGetGeoJSON()
+    {
+        $index = new PersistentIndex(['my_persistent_attr'], false, false);
+        $this->assertFalse($index->isUnique());
+        $this->assertFalse($index->isSparse());
+    }
+
+    public function testGetCreateData()
+    {
+        $index = new PersistentIndex(['my_persistent_attr']);
+        $this->assertCount(4, $index->getCreateData());
+        $data = $index->getCreateData();
+        $this->assertArrayHasKey('type', $data);
+        $this->assertArrayHasKey('fields', $data);
+        $this->assertArrayHasKey('unique', $data);
+        $this->assertArrayHasKey('sparse', $data);
+    }
+}
