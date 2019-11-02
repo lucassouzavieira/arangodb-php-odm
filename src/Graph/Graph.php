516b08e6da1bd605dd1321f82a778e20ab0a4588
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2019-11-02 18:17:47 -0300

    Start to implements Graphs feature

---
+++ src/Graph/Graph.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace ArangoDB\Graph;
+
+/**
+ * Represents an ArangoDB Graph
+ *
+ * @package ArangoDB\Graph
+ * @author Lucas S. Vieira
+ */
+class Graph
+{
+}
