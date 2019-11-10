0d1a63f4cc1127b93edd4dd1227a81d1541274e0
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2019-11-09 21:52:09 -0300

    Add some 'Graph' class unit tests

--- tests/Graph/GraphTest.php
+++ tests/Graph/GraphTest.php
@@ -412,6 +412,47 @@ class GraphTest extends TestCase
         $this->assertArrayHasKey('edgeDefinitions', $arr);
     }
 
+    public function testAddEdgeDefinition()
+    {
+        $db = $this->getConnectionObject()->getDatabase();
+        $graph = new Graph("my_graph", $this->mockGraphAttributes(), $db);
+
+        $graph->save();
+
+        $this->assertCount(1, $graph->getEdgeDefinitions());
+        $graph->addEdgeDefinition('edgeCollB', ['coll_x'], ['coll_y']);
+
+        $graphObj = $db->getGraph('my_graph');
+        $this->assertCount(2, $graphObj->getEdgeDefinitions());
+        $this->assertTrue($graphObj->delete(true));
+    }
+
+    public function testAddEdgeDefinitionThrowDatabaseExceptionOnNonDefinedDatabase()
+    {
+        $db = $this->getConnectionObject()->getDatabase();
+        $graph = new Graph("my_graph", $this->mockGraphAttributes(true));
+
+        $this->expectException(DatabaseException::class);
+        $this->expectExceptionMessage("Database not defined");
+        $graph->addEdgeDefinition('edge_coll', ['coll_c'], ['coll_d']);
+    }
+
+
+    public function testAddEdgeDefinitionThrowDatabaseException()
+    {
+        $mock = new MockHandler([
+            new Response(200, [], json_encode(['result' => []])),
+            new Response(200, [], json_encode(['result' => []])),
+            new Response(403, [], json_encode($this->mockServerError()))
+        ]);
+
+        $db = $this->getConnectionObject($mock)->getDatabase();
+        $graph = new Graph("my_graph", $this->mockGraphAttributes(true), $db);
+
+        $this->expectException(DatabaseException::class);
+        $graph->addEdgeDefinition('edge_coll', ['coll_c'], ['coll_d']);
+    }
+
     public function testAddEdgeDefinitionToNewGraph()
     {
         $graph = new Graph("my_graph", $this->mockGraphAttributes());
